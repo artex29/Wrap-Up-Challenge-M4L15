1e4a7884dbd67d65aa7cc93eafb3fee498a40db2
Angel <angelramirez@artexcomputer.com>
2021-12-31 11:29:10 -0500

    Initial Commit

---
+++ Wrap Up Challenge Books M4L15/Wrap_Up_Challenge_Books_M4L15App.swift
@@ -0,0 +1,17 @@
+//
+//  Wrap_Up_Challenge_Books_M4L15App.swift
+//  Wrap Up Challenge Books M4L15
+//
+//  Created by ANGEL RAMIREZ on 12/31/21.
+//
+
+import SwiftUI
+
+@main
+struct Wrap_Up_Challenge_Books_M4L15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
